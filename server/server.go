ae3140227a68148107fcfe5fa0ff74cafe265176
JunYanBJSS <johnnycocoyan@hotmail.com>
2024-05-27 22:50:19 +0100

    add worker pool

--- server/server.go
+++ server/server.go
@@ -7,8 +7,51 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
+var peopleMap = make(map[string]int)
+var messageChan = make(chan message)
+
+type WorkerPool interface {
+	Run()
+	AddTask()
+}
+type job struct {
+	Request  *http.Request
+	Response http.ResponseWriter
+}
+
+type workerPool struct {
+	maxWorker int
+	jobQueue  chan job
+}
+
+func NewWorkerPool(maxWorker int) *workerPool {
+	return &workerPool{
+		maxWorker: maxWorker,
+		jobQueue:  make(chan job),
+	}
+}
+
+func (wp *workerPool) Run() {
+	for i := 0; i < wp.maxWorker; i++ {
+		go func() {
+			for job := range wp.jobQueue {
+				handleRequest(job.Response, job.Request)
+			}
+		}()
+	}
+}
+
+func (wp *workerPool) AddTask(task *job) {
+	wp.jobQueue <- *task
+}
+
+type People struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 type message struct {
 	command  string
 	key      string
@@ -16,82 +59,76 @@ type message struct {
 	response chan<- string
 }
 
-type mapActor struct {
-	peopleMap map[string]int
-	message   chan message
-}
-
-func newActor() *mapActor {
-	return &mapActor{
-		peopleMap: make(map[string]int),
-		message:   make(chan message),
-	}
-}
-
-func (m *mapActor) run() {
-	for mes := range m.message {
+func run() {
+	for mes := range messageChan {
 		switch mes.command {
 		case "get":
 			res := "{"
-			for k, v := range m.peopleMap {
+			for k, v := range peopleMap {
 				res += "\n" + k + ": " + strconv.Itoa(v)
 			}
 			res += "\n}"
 			mes.response <- res
 		case "put":
-			m.peopleMap[mes.key] = mes.value
+			peopleMap[mes.key] = mes.value
 			mes.response <- fmt.Sprintf("%s:%d", mes.key, mes.value)
 		case "delete":
-			delete(m.peopleMap, mes.key)
+			delete(peopleMap, mes.key)
 			mes.response <- fmt.Sprintf("%s has deleted", mes.key)
 		}
 	}
 }
-
-type People struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Path[len("/delete/"):]
+	if name == "" {
+		http.Error(w, "empty string", http.StatusBadRequest)
+	}
+	res := make(chan string)
+	defer close(res)
+	messageChan <- message{command: "delete", key: name, response: res}
+	fmt.Fprintln(w, <-res)
 }
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	if strings.Contains(r.URL.Path, "/delete/") {
+		handleDelete(w, r)
+		return
+	}
+	response := make(chan string)
+	defer close(response)
+	switch r.Method {
+	case "GET":
+		func(w http.ResponseWriter, r *http.Request) {
 
+			messageChan <- message{command: "get", response: response}
+			fmt.Fprintln(w, <-response)
+		}(w, r)
+	case "POST":
+		func(w http.ResponseWriter, r *http.Request) {
+			people := requestDecoder(w, r)
+			messageChan <- message{command: "put", key: people.Name, value: people.Age, response: response}
+			fmt.Fprintln(w, <-response)
+		}(w, r)
+	case "PUT":
+		func(w http.ResponseWriter, r *http.Request) {
+			people := requestDecoder(w, r)
+			messageChan <- message{command: "put", key: people.Name, value: people.Age, response: response}
+			fmt.Fprintln(w, <-response)
+		}(w, r)
+	default:
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+}
 func NewServer() {
+	workerPool := NewWorkerPool(10)
+	workerPool.Run()
 
-	mapActor := newActor()
-	go mapActor.run()
+	go run()
 	server := &http.Server{Addr: ":8080"}
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case "GET":
-			func(w http.ResponseWriter, r *http.Request) {
-				response := make(chan string)
-				mapActor.message <- message{command: "get", response: response}
-				fmt.Fprintln(w, <-response)
-			}(w, r)
-		case "POST":
-			func(w http.ResponseWriter, r *http.Request) {
-				people := requestDecoder(w, r)
-				response := make(chan string)
-				mapActor.message <- message{command: "put", key: people.Name, value: people.Age, response: response}
-				fmt.Fprintln(w, <-response)
-			}(w, r)
-		case "PUT":
-			func(w http.ResponseWriter, r *http.Request) {
-				people := requestDecoder(w, r)
-				response := make(chan string)
-				mapActor.message <- message{command: "put", key: people.Name, value: people.Age, response: response}
-				fmt.Fprintln(w, <-response)
-			}(w, r)
-		default:
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-		}
+		workerPool.AddTask(&job{Request: r, Response: w})
 	})
 	http.HandleFunc("/delete/", func(w http.ResponseWriter, r *http.Request) {
-		name := r.URL.Path[len("/delete/"):]
-		if name == "" {
-			http.Error(w, "empty string", http.StatusBadRequest)
-		}
-		res := make(chan string)
-		mapActor.message <- message{command: "delete", key: name, response: res}
-		fmt.Fprintln(w, <-res)
+		workerPool.AddTask(&job{Request: r, Response: w})
 	})
 	terminate := make(chan os.Signal, 1)
 	go func() {
